20d777e575fb1ff85fddbe76faf0256ab8944c81
AlphaConseils <alpha@alphamada.com>
2022-05-10 17:43:35 +0200

    FIX[s2pc_pos] : add lot number on invoie BLF

--- s2pc_pos/__manifest__.py
+++ s2pc_pos/__manifest__.py
@@ -1,53 +1,53 @@
 # -*- coding: utf-8 -*-
 {
-	'name': "s2pc_pos",
+    'name': "s2pc_pos",
 
-	'summary': """ S2PC Pos""",
+    'summary': """ S2PC Pos""",
 
-	'description': """
+    'description': """
 
 	""",
 
-	'author': "Alpha conseils",
-	'website': "https://www.alphamada.com/",
+    'author': "Alpha conseils",
+    'website': "https://www.alphamada.com/",
 
-	# Categories can be used to filter modules in modules listing
-	# Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-	# for the full list
-	'category': 'Uncategorized',
-	'version': '0.1',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
 
-	# any module necessary for this one to work correctly
-	'depends': [
-				'point_of_sale',
-				's2pc_base',
-				's2pc_contact',
-				's2pc_product',
-	],
+    # any module necessary for this one to work correctly
+    'depends': [
+        'point_of_sale',
+        's2pc_base',
+        's2pc_contact',
+        's2pc_product',
+        'sale_stock',
+    ],
 
-	# always loaded
-	'data': [
-		# security
+    # always loaded
+    'data': [
+        # security
 
-		# views
-		'views/account_invoice_pos_template.xml',
-		'views/account_invoice_pos_report.xml',
-	],
-	# only loaded in demonstration mode
-	'demo': [
-	],
-	'installable': True,
-	'application': False,
-	'auto_install': False,
-	'assets': {
-		'point_of_sale.assets': [
-			's2pc_pos/static/src/js/models.js',
+        # views
+        'views/account_invoice_pos_template.xml',
+        'views/account_invoice_pos_report.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'assets': {
+        'point_of_sale.assets': [
+            's2pc_pos/static/src/js/models.js',
 
-
-		],
-		'web.report_assets_common': [
-			's2pc_pos/static/src/css/account_invoice_pos.css',
-		],
-	},
-	'license': 'LGPL-3',
+        ],
+        'web.report_assets_common': [
+            's2pc_pos/static/src/css/account_invoice_pos.css',
+        ],
+    },
+    'license': 'LGPL-3',
 }
--- s2pc_pos/models/account_move.py
+++ s2pc_pos/models/account_move.py
@@ -4,20 +4,46 @@ from odoo.addons.s2pc_base.models.tools import amount_to_text_fr
 
 
 class AccountMove(models.Model):
-	_inherit = 'account.move'
+    _inherit = 'account.move'
 
-	amount_total_text = fields.Char('Amount total text', compute='compute_amount_to_text')
+    amount_total_text = fields.Char('Amount total text', compute='compute_amount_to_text')
 
-	def compute_amount_to_text(self):
-		for rec in self:
-			rec.amount_total_text = amount_to_text_fr(rec.amount_total, rec.currency_id.currency_unit_label)
+    def compute_amount_to_text(self):
+        for rec in self:
+            rec.amount_total_text = amount_to_text_fr(rec.amount_total, rec.currency_id.currency_unit_label)
+
+    def get_lot_number(self, name, product_id):
+        """
+            get BL id
+        """
+        id_bl = []
+        lot = ""
+
+        bc = self.env['sale.order'].search([('name', '=', name)])
+        pc = self.env['pos.order'].search([('name', '=', name)])
+
+        if bc[0]:
+            bl = self.env['stock.picking'].search([('origin', '=', bc.name)])
+        else:
+            bl = self.env['stock.picking'].search([('origin', '=', pc.name)])
+
+        for i in bl:
+            id_bl.append(i)
+        if id_bl[0]:
+            for line in id_bl[0].move_ids_without_package:
+                if line.product_id == product_id:
+                    lot = line.lot_ids[0].name
+        else:
+            lot = ""
+
+        return lot
 
 
 class AccountMoveLine(models.Model):
-	_inherit = 'account.move.line'
+    _inherit = 'account.move.line'
 
-	weight = fields.Float('Total weight', compute='compute_weight')
+    weight = fields.Float('Total weight', compute='compute_weight')
 
-	def compute_weight(self):
-		for rec in self:
-			rec.weight = rec.product_id.weight or 0 * rec.quantity
+    def compute_weight(self):
+        for rec in self:
+            rec.weight = rec.product_id.weight or 0 * rec.quantity
