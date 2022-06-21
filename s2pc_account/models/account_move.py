73e443d32e2e1c94619e3f520227924b2908da1b
AlphaConseils <alpha@alphamada.com>
2022-06-21 18:46:34 +0200

    FEAT[sp2c_account, pos] : Add reglement mode on account move, add date bold, name bold and journal_id code

--- s2pc_account/models/account_move.py
+++ s2pc_account/models/account_move.py
@@ -4,38 +4,45 @@ from odoo import models, fields, api
 
 
 class AccountMove(models.Model):
-	_inherit = "account.move"
+    _inherit = "account.move"
 
-	advised_pricelist_id = fields.Many2one('product.pricelist', 'Advised pricelist', compute='_compute_advised_pricelist')
+    advised_pricelist_id = fields.Many2one('product.pricelist', 'Advised pricelist',
+                                           compute='_compute_advised_pricelist')
+    reglement_mode = fields.Selection(
+        string='Mode de règlement',
+        selection=[('bnq', 'BNQ'),
+                   ('esp', 'ESP'), ('chk', 'CHQ'), ],
+        default='esp', )
 
-	def _compute_advised_pricelist(self):
-		for rec in self:
-			partner = rec.partner_id
-			parent = partner.parent_id
-			pricelist = partner.advised_pricelist_id or parent.advised_pricelist_id
-			if pricelist:
-				rec.advised_pricelist_id = pricelist.id
-			else:
-				rec.advised_pricelist_id = False
+    def _compute_advised_pricelist(self):
+        for rec in self:
+            partner = rec.partner_id
+            parent = partner.parent_id
+            pricelist = partner.advised_pricelist_id or parent.advised_pricelist_id
+            if pricelist:
+                rec.advised_pricelist_id = pricelist.id
+            else:
+                rec.advised_pricelist_id = False
 
 
 class AccountMoveLine(models.Model):
-	_inherit = 'account.move.line'
+    _inherit = 'account.move.line'
 
-	advised_price_unit = fields.Monetary('Advised price unit', compute='_compute_advised_price_unit')
-	advised_ratio = fields.Float('Advised price ratio', compute='_compute_advised_ratio')
+    advised_price_unit = fields.Monetary('Advised price unit', compute='_compute_advised_price_unit')
+    advised_ratio = fields.Float('Advised price ratio', compute='_compute_advised_ratio')
 
-	def _compute_advised_price_unit(self):
-		for rec in self:
-			advised_pricelist = rec.move_id.advised_pricelist_id
-			if advised_pricelist:
-				rec.advised_price_unit = advised_pricelist.price_get(rec.product_id.id, rec.quantity).get(advised_pricelist.id)
-			else:
-				rec.advised_price_unit = 0
+    def _compute_advised_price_unit(self):
+        for rec in self:
+            advised_pricelist = rec.move_id.advised_pricelist_id
+            if advised_pricelist:
+                rec.advised_price_unit = advised_pricelist.price_get(rec.product_id.id, rec.quantity).get(
+                    advised_pricelist.id)
+            else:
+                rec.advised_price_unit = 0
 
-	def _compute_advised_ratio(self):
-		for rec in self:
-			if rec.price_unit and rec.advised_price_unit:
-				rec.advised_ratio = (rec.advised_price_unit - rec.price_unit) * 100 / rec.advised_price_unit
-			else:
-				rec.advised_ratio = 0
+    def _compute_advised_ratio(self):
+        for rec in self:
+            if rec.price_unit and rec.advised_price_unit:
+                rec.advised_ratio = (rec.advised_price_unit - rec.price_unit) * 100 / rec.advised_price_unit
+            else:
+                rec.advised_ratio = 0
