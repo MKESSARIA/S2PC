01641d62998b39a21f6dc5512a4b52b79100afeb
rivo2302 <rivo2302@gmail.com>
2022-06-30 13:51:33 +0300

    ADD [s2pc_account] fields ville on account move

--- s2pc_account/models/account_move.py
+++ s2pc_account/models/account_move.py
@@ -6,13 +6,27 @@ from odoo import models, fields, api
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    advised_pricelist_id = fields.Many2one('product.pricelist', 'Advised pricelist',
-                                           compute='_compute_advised_pricelist')
+    advised_pricelist_id = fields.Many2one(
+        "product.pricelist", "Advised pricelist", compute="_compute_advised_pricelist"
+    )
     reglement_mode = fields.Selection(
-        string='Mode de règlement',
-        selection=[('bnq', 'BNQ'),
-                   ('esp', 'ESP'), ('chk', 'CHQ'), ],
-        default='esp', )
+        string="Mode de règlement",
+        selection=[
+            ("bnq", "BNQ"),
+            ("esp", "ESP"),
+            ("chk", "CHQ"),
+        ],
+        default="esp",
+    )
+
+    ville = fields.Selection(
+        [
+            ("tana", "Tananarive"),
+            ("tamatave", "Tamatave"),
+        ],
+        string="La ville où  a été fait la facture",
+        help="Pour mieux faire les recherches il faut  completér ce champs Ville.",
+    )
 
     def _compute_advised_pricelist(self):
         for rec in self:
@@ -26,23 +40,32 @@ class AccountMove(models.Model):
 
 
 class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
-    advised_price_unit = fields.Monetary('Advised price unit', compute='_compute_advised_price_unit')
-    advised_ratio = fields.Float('Advised price ratio', compute='_compute_advised_ratio')
+    advised_price_unit = fields.Monetary(
+        "Advised price unit", compute="_compute_advised_price_unit"
+    )
+    advised_ratio = fields.Float(
+        "Advised price ratio", compute="_compute_advised_ratio"
+    )
 
     def _compute_advised_price_unit(self):
         for rec in self:
             advised_pricelist = rec.move_id.advised_pricelist_id
             if advised_pricelist:
-                rec.advised_price_unit = advised_pricelist.price_get(rec.product_id.id, rec.quantity).get(
-                    advised_pricelist.id)
+                rec.advised_price_unit = advised_pricelist.price_get(
+                    rec.product_id.id, rec.quantity
+                ).get(advised_pricelist.id)
             else:
                 rec.advised_price_unit = 0
 
     def _compute_advised_ratio(self):
         for rec in self:
             if rec.price_unit and rec.advised_price_unit:
-                rec.advised_ratio = (rec.advised_price_unit - rec.price_unit) * 100 / rec.advised_price_unit
+                rec.advised_ratio = (
+                    (rec.advised_price_unit - rec.price_unit)
+                    * 100
+                    / rec.advised_price_unit
+                )
             else:
                 rec.advised_ratio = 0
--- s2pc_account/models/account_move_invoice.py
+++ s2pc_account/models/account_move_invoice.py
@@ -6,19 +6,21 @@ from odoo.exceptions import UserError
 
 
 class AccountInvoiceReport(models.AbstractModel):
-    _name = 'report.s2pc_account.temp'
-    _description = 'Invoice Report'
+    _name = "report.s2pc_account.temp"
+    _description = "Invoice Report"
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        moves = self.env['account.move'].sudo().browse(docids)
-        lines = moves.mapped('invoice_line_ids')
+        moves = self.env["account.move"].sudo().browse(docids)
+        lines = moves.mapped("invoice_line_ids")
         range = len(lines) if len(lines) > 12 else 12
 
         return {
-            'docs': moves,
-            'line_range': range,
-            'invoice_lines': lines,
-            'invoice_count': len(lines),
-            'qr_code_urls': self.env['report.account.report_invoice'].sudo()._get_report_values(docids)['qr_code_urls']
+            "docs": moves,
+            "line_range": range,
+            "invoice_lines": lines,
+            "invoice_count": len(lines),
+            "qr_code_urls": self.env["report.account.report_invoice"]
+            .sudo()
+            ._get_report_values(docids)["qr_code_urls"],
         }
