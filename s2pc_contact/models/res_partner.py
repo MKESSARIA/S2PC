a4457d126d68d20891e5e5744e6620dbc6b162ff
AlphaConseils <alpha@alphamada.com>
2022-07-22 17:31:50 +0200

    [FEAT]s2pc_contact : Add responsible salesperson

--- s2pc_contact/models/res_partner.py
+++ s2pc_contact/models/res_partner.py
@@ -10,6 +10,9 @@ class ResPartner(models.Model):
     RCS = fields.Char('RCS', index=1)
     CIF = fields.Char('CIF', index=1)
     advised_pricelist_id = fields.Many2one('product.pricelist', 'Advised Pricelist')
+    responsible_salesperson_id = fields.Many2one('res.users', string='Vendeur responsable',
+                              help="L'utilisateur interne chargé de ce contact.")
+
 
     @api.model
     def get_parent(self):
